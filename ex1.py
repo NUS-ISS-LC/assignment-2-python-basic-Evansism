849e76d1144897f6655b319371a9197b9a9a0249
You Yihong <3van5hop3@gmail.com>
2025-11-10 01:15:20 +0000

    complete

--- ex1.py
+++ ex1.py
@@ -1,3 +1,11 @@
 def find(s, n):
-# write your implementation here
+
+    for i in range(len(s)):
+        for j in range(len(s)):
+            if i == j:
+                continue
+            elif s[i] + s[j] == n:
+                return [i,j]
     return None
+
+print(find([3,2,4],6))
